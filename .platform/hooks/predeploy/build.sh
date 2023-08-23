3872b4133833760e8ced4c655835bc5b2f7ac07e
weddleben <stomachworm88@gmail.com>
2023-08-23 14:54:45 -0500

    comment bash file

--- .platform/hooks/predeploy/build.sh
+++ .platform/hooks/predeploy/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo amazon-linux-extras install epel
+# this version is reallyyyyy important.
+# don't change unless you've a great reason to do so
 sudo yum install -y chromium-114.0.5735.198-1.el7.x86_64
