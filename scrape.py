98247d680e14669eb692381f8e3c05bd896c824b
weddleben <stomachworm88@gmail.com>
2023-09-05 16:36:47 -0500

    slow progress scraping the new site...

--- scrape.py
+++ scrape.py
@@ -5,6 +5,7 @@
 
 import os
 from datetime import datetime
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -21,7 +22,7 @@ def scrape():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--window-size=1420,1080')
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
 
     '''
@@ -37,10 +38,18 @@ def scrape():
     else:
         driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM, cache_valid_range=300, version='114.0.5735.16').install()), options=chrome_options)
 
-    driver.get('https://www.volgistics.com/ex/portal.dll/?FROM=15495')
+    driver.get('https://www.volgistics.com/vicnet/15495/schedule')
 
-    email = driver.find_element(by=By.NAME, value="LN")
-    password = driver.find_element(by=By.NAME, value="PW")
+    '''
+    COME UP WITH SOMETHING ELSE FOR THIS BELOW!!!
+    '''
+    time.sleep(2)
+    '''
+    COME UP WITH SOMETHING ELSE FOR THIS ABOVE!!!
+    '''
+
+    email = driver.find_element(by=By.NAME, value="email")
+    password = driver.find_element(by=By.NAME, value="password")
 
     VIC_user = os.environ['VIC_user']
     VIC_pass = os.environ['VIC_password']
@@ -48,13 +57,10 @@ def scrape():
     email.send_keys(VIC_user)
     password.send_keys(VIC_pass)
 
-    submit = driver.find_element(by=By.NAME, value="Go")
-    submit.click()
+    # submit = driver.find_element(by=By.CLASS_NAME, value="mat-mdc-raised-button")
+    # submit.click()
 
     # now we're logged in
-
-    my_schedule = driver.find_element(by=By.NAME, value='Sch')
-    my_schedule.click()
 
     '''this is so ugly. TODO REFACTOR so it makes sense!'''
     days = driver.find_elements(By.CLASS_NAME, value='a')
