cfa858862560f43e75499b38bf9c96f1b30e72e3
weddleben <stomachworm88@gmail.com>
2023-09-07 16:05:59 -0500

    seems to be working OK, turning headless mode off

--- scrape.py
+++ scrape.py
@@ -22,7 +22,7 @@ def scrape():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--window-size=1420,1080')
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
 
     '''
