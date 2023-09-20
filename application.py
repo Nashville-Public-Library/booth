ebd7e6dead0bab6567d148574f6b9040f825d6ad
weddleben <stomachworm88@gmail.com>
2023-09-20 16:10:48 -0500

    useful comment
    AWS EB nginx defaults to looking for a flask object called application.
    
    you can change it via a config file, but this is simpler

--- application.py
+++ application.py
@@ -3,6 +3,7 @@
 © Ben Weddle is to blame for this code. Anyone is free to use it.
 '''
 
+# AWS EB default is to look for flask instance variable called application.
 from app import app as application
 
 if __name__ == '__main__':
