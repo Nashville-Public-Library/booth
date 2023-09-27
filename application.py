cd7d6527ef75bb12be441d51b29aa20d9c9a422a
weddleben <stomachworm88@gmail.com>
2023-09-27 17:01:20 -0500

    whoops

--- application.py
+++ application.py
@@ -7,7 +7,8 @@ import os
 # AWS EB default is to look for flask instance variable called application.
 from app import app as application
 
-if 'nt' in os.name:
-    application.run(debug=True)
-else:
-    application.run()
+if __name__ == "__main__":
+    if 'nt' in os.name:
+        application.run(debug=True)
+    else:
+        application.run()
