4bc1453622bbd5c6f78a08a681d7f7fcfb50d2d0
weddleben <stomachworm88@gmail.com>
2023-09-27 15:52:48 -0500

    turn on debug mode permanently for dev
    will never ever deploy this to prod on a windows machine so I think this is safe to do...

--- application.py
+++ application.py
@@ -2,9 +2,12 @@
 © Nashville Public Library
 © Ben Weddle is to blame for this code. Anyone is free to use it.
 '''
+import os
 
 # AWS EB default is to look for flask instance variable called application.
 from app import app as application
 
-if __name__ == '__main__':
+if 'nt' in os.name:
+    application.run(debug=True)
+else:
     application.run()
