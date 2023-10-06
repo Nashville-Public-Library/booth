175c89e7db6c9a4e496cb677b8a8e4f22adc97c4
weddleben <stomachworm88@gmail.com>
2023-10-06 13:48:41 -0500

    adjust routes and remove stream routes

--- app/booth/views.py
+++ app/booth/views.py
@@ -3,10 +3,9 @@ from flask import render_template, request, make_response
 from app import app
 from app.utils import are_we_closed, check_banner
 from app.booth.hours import hour1, hour2
-from app.booth.icecast import icecast_now_playing
 from app.booth.scrape import scrape
 
-@app.route('/live')
+@app.route('/booth/live')
 def homepage():
     if are_we_closed():
         return render_template('closed.html')
@@ -23,7 +22,7 @@ def health_check():
         return 'la dee dah'
     return "<div style='font-size: 85pt; text-align: center;'>I AM WORKING FINE</div>" 
 
-@app.route('/')
+@app.route('/booth/')
 def dot():
     if are_we_closed():
         return render_template('closed.html')
@@ -34,12 +33,12 @@ def dot():
 just using this route for testing styles and such so we don't
 need to run selenium every time we want to reload the page.
 '''
-@app.route('/test')
+@app.route('/booth/test')
 def testing():
     return render_template('home.html', booth1='Test Nobody', booth2='Test Nobody', booth3='Test Nobody',\
         booth1_2='Test Nobody', booth2_2='Test Nobody', booth3_2='Test Nobody', hour=hour1(), hour2=hour2(), banner=check_banner())
 
-@app.route('/banner', methods=['GET', 'POST'])
+@app.route('/booth/banner', methods=['GET', 'POST'])
 def banner():
     if request.method == 'POST':
         password = request.form['password']
@@ -51,16 +50,6 @@ def banner():
         else:
             return render_template('banner.html', emoji='&#128078;', banner_text=check_banner()) # emoji thumbs down
     return render_template('banner.html', banner_text=check_banner())
-
-@app.route('/stream')
-def now_playing():
-    icecast = {'nowPlaying': icecast_now_playing()}
-    response = make_response(icecast)
-    response.headers['customHeader'] = 'Darth Vader'
-    response.status_code = 200
-    response.content_type = 'application/json'
-    response.access_control_allow_origin = '*'
-    return response
 
 
 # do something to explicitly handle HTTP errors so we don't get some general nginx page
