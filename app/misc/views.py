affe052a6537118ac8edfb5cac5090250b250281
weddleben <stomachworm88@gmail.com>
2023-10-12 14:55:34 -0500

    rename errorhandler functions

--- app/misc/views.py
+++ app/misc/views.py
@@ -11,12 +11,12 @@ def home():
 # do something to explicitly handle HTTP errors so we don't get some general nginx page
 
 @app.errorhandler(404)
-def page_not_found(e):
+def not_found(e):
     return render_template('404.html', e=e), 404
 
 @app.errorhandler(405)
-def page_not_found(e):
-    return "you're no good, you're no good"
+def not_allowed(e):
+    return "you're no good, you're no good", 405
 
 @app.errorhandler(500)
 def handle_exception(e):
