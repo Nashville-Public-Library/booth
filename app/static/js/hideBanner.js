aac42b169aeacf2021d7e5478ad049ee7a0eed94
weddleben <stomachworm88@gmail.com>
2023-10-17 10:10:05 -0500

    move BrightSign hide banner to separate file
    for the BrightSign, if there is a banner message, we need to remove the footer, since there isn't enough room on the screen.
    
    after the recent directory structure changes, for whatever reason, the JS to hide the footer stopped working. this is a fix for that.

---
+++ app/static/js/hideBanner.js
@@ -0,0 +1,18 @@
+/*
+If banner element is on the page, remove footer/images BUT only for BrightSign, 
+which we target below. We blew up the font for BrightSign so big (staff request) that
+we can't fit everything on the screen.
+ 
+This feels hacky, sorry.
+*/
+
+window.onload = hide_footer()
+
+function hide_footer() {
+    is_BrightSign = window.navigator.userAgent.includes('Mozilla');
+
+    if (is_BrightSign) {
+        console.log(window.navigator.userAgent)
+        document.getElementById('footer').style.visibility = 'hidden';
+    }
+}
