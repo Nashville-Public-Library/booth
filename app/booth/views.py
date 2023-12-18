1504acb1babfa17fc464f061f4fcbf609fda985d
weddleben <stomachworm88@gmail.com>
2023-12-18 16:10:29 -0600

    cleaning up

--- app/booth/views.py
+++ app/booth/views.py
@@ -17,27 +17,16 @@ def dot():
 @app.route('/booth/data', methods=['POST'])
 def homepage():
     response = make_response(get_scrape_and_filter())
-    # response = {'booth1_1': 'mah', 'booth2_1': 'mahh', 'booth3_1': 'mahhh', 'booth1_2': 'mahhhh', 'booth2_2': 'mahhh', 'booth3_2': 'mahhh'}
     response = make_response(response)
     response.headers['customHeader'] = 'Darth Vader'
     response.status_code = 200
     response.content_type = 'application/json'
     response.access_control_allow_origin = '*'
-    print(response)
     return response
 
 @app.route('/health', methods=['GET', 'POST'])
 def health_check():
     return "<div style='font-size: 85pt; text-align: center;'>I AM WORKING FINE</div>" 
-
-'''
-just using this route for testing styles and such so we don't
-need to run selenium every time we want to reload the page.
-'''
-@app.route('/booth/test')
-def testing():
-    return render_template('booth.html', booth1='Test Nobody', booth2='Test Nobody', booth3='Test Nobody',\
-        booth1_2='Test Nobody', booth2_2='Test Nobody', booth3_2='Test Nobody', hour=hour1(), hour2=hour2(), banner=check_banner())
 
 @app.route('/booth/banner', methods=['GET', 'POST'])
 def banner():
