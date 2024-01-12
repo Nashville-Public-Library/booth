2314d65a74f10d91b350ce0f1f8eb1176dacc203
weddleben <stomachworm88@gmail.com>
2024-01-12 14:11:24 -0600

    fix http return code mismatch

--- app/misc/views.py
+++ app/misc/views.py
@@ -15,8 +15,8 @@ def health_check():
 # do something to explicitly handle HTTP errors so we don't get some general nginx page
 
 @app.errorhandler(400)
-def not_allowed(e):
-    return "you're no good, you're no good", 405
+def bad_request(e):
+    return "you're no good, you're no good", 400
 
 @app.errorhandler(404)
 def not_found(e):
