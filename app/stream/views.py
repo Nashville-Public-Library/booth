31710f8b6aad333862e727ed60c8a1506c3b80f4
weddleben <stomachworm88@gmail.com>
2024-03-05 14:20:08 -0600

    update route to match previous commit

--- app/stream/views.py
+++ app/stream/views.py
@@ -1,12 +1,11 @@
 from flask import make_response, redirect
 
 from app import app
-from app.stream.icecast import icecast_now_playing
+from app.stream.icecast import Icecast
 
 @app.route('/stream', methods=['POST'])
 def now_playing():
-    icecast = {'nowPlaying': icecast_now_playing()}
-    response = make_response(icecast)
+    response = make_response(Icecast().now_playing)
     response.headers['customHeader'] = 'Darth Vader'
     response.status_code = 200
     response.content_type = 'application/json'
