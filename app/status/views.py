3121ba512c48a51c69deef962cb3f41b6c471c9b
weddleben <stomachworm88@gmail.com>
2024-04-29 15:05:32 -0500

    testing #60...why is this so hard

--- app/status/views.py
+++ app/status/views.py
@@ -20,9 +20,9 @@ def requires_auth(mah):
         ip = request.headers.get('X-Forwarded-For', request.remote_addr)
         if type(ip) == tuple:
              if ip[0] in ('170.190.43.1', '127.0.0.1'):
-                return mah()
+                return f"tuple type: ip: {ip}, ip[0]: {ip[0]}"
         if ip == '170.190.43.1' or ip == '127.0.0.1':
-             return mah()
+             return f"tuple type: ip: {ip}, ip[0]: {ip[0]}"
         auth = request.authorization
         if not auth or not check_auth(auth.username, auth.password):
             return authenticate()
