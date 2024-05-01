23bd99f035c51ce23e52aa5ecd0d61cff6e993fe
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2024-05-01 11:47:52 -0500

    next attempt

--- .platform/hooks/predeploy/build.sh
+++ .platform/hooks/predeploy/build.sh
@@ -3,3 +3,19 @@ sudo amazon-linux-extras install epel
 # this version is reallyyyyy important.
 # don't change unless you've a great reason to do so
 sudo yum install -y chromium
+
+# Directory containing Chromedriver
+chromedriver_dir="/var/app/current/chromedriver"
+
+# Check if the directory exists
+if [ -d "$chromedriver_dir" ]; then
+    # Add directory to PATH if it's not already there
+    if [[ ":$PATH:" != *":$chromedriver_dir:"* ]]; then
+        export PATH="$chromedriver_dir:$PATH"
+        echo "Chromedriver directory added to PATH."
+    else
+        echo "Chromedriver directory is already in PATH."
+    fi
+else
+    echo "Chromedriver directory not found: $chromedriver_dir"
+fi
