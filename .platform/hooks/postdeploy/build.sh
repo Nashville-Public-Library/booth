74842fed8d61cbecc161b36fdd2420c36609b6c1
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2024-05-01 16:53:39 -0500

    update again

--- .platform/hooks/postdeploy/build.sh
+++ .platform/hooks/postdeploy/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# # Change ownership of chromedriver and its containing directory
-# sudo chown -R webapp:webapp /var/app/current/chromedriver
+# Change ownership of chromedriver and its containing directory
+sudo chown -R webapp:webapp /var/app/current/chromedriver
 
-# # Set permissions for chromedriver
-# sudo chmod 755 /var/app/current/chromedriver
+# Set permissions for chromedriver
+sudo chmod 755 /var/app/current/chromedriver
