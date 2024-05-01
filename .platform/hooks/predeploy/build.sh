06787fe258e9a99e5151455118fd115eaeaf5676
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2024-05-01 18:07:00 -0500

    no

--- .platform/hooks/predeploy/build.sh
+++ .platform/hooks/predeploy/build.sh
@@ -20,7 +20,7 @@ wget https://storage.googleapis.com/chrome-for-testing-public/124.0.6367.91/linu
 unzip chrome-linux64.zip
 echo "make dir"
 sudo mkdir -p /opt/google-chrome
-sudo mv chrome-linux64 /opt/google/chrome
+sudo mv chrome-linux64 /opt/google-chrome
 sudo ln -s /opt/google-chrome/chrome /usr/local/bin/chrome
 
 # Change ownership of chromedriver and its containing directory
