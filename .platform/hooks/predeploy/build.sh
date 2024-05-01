7a69a7db6e9fb7fb863c0ec70bbc1d5cbd68c742
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2024-05-01 17:21:52 -0500

    redeploy

--- .platform/hooks/predeploy/build.sh
+++ .platform/hooks/predeploy/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Install dependencies required for Chrome
-sudo yum install -y \
-    libX11 \
-    GConf2 \
-    alsa-lib \
-    atk \
-    gtk3 \
-    ipa-gothic-fonts \
-    xorg-x11-fonts-100dpi \
-    xorg-x11-fonts-75dpi \
-    xorg-x11-utils \
-    xorg-x11-fonts-cyrillic \
-    xorg-x11-fonts-Type1 \
-    xorg-x11-fonts-misc
+# # Install dependencies required for Chrome
+# sudo yum install -y \
+#     libX11 \
+#     GConf2 \
+#     alsa-lib \
+#     atk \
+#     gtk3 \
+#     ipa-gothic-fonts \
+#     xorg-x11-fonts-100dpi \
+#     xorg-x11-fonts-75dpi \
+#     xorg-x11-utils \
+#     xorg-x11-fonts-cyrillic \
+#     xorg-x11-fonts-Type1 \
+#     xorg-x11-fonts-misc
 
 # Download and install Chrome
 wget https://storage.googleapis.com/chrome-for-testing-public/124.0.6367.91/linux64/chrome-linux64.zip
@@ -26,5 +26,3 @@ sudo chown -R webapp:webapp /var/app/current/chromedriver
 
 # Set permissions for chromedriver
 sudo chmod 755 /var/app/current/chromedriver
-
-#redeploy
