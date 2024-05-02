a1be461c5cacf3b3a53d061312fa7d2b5a0666a2
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2024-05-01 22:20:34 -0500

    no longer needed (i hope)

--- .platform/hooks/postdeploy/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Change ownership of chromedriver and its containing directory
-sudo chown -R webapp:webapp /var/app/current/chromedriver
-
-# Set permissions for chromedriver
-sudo chmod 755 /var/app/current/chromedriver
