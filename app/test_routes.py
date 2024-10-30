2282d5a16cb767bab48f17b830451e0cfcc9f046
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2024-10-30 13:32:51 -0500

    remove EV patch, rearrange/add  new tests

--- app/test_routes.py
+++ app/test_routes.py
@@ -8,20 +8,15 @@ from flask.testing import FlaskClient
 @pytest.fixture
 def client():
     app.testing = True
-    env_vars = {
-        'VIC_user': 'nothing',
-        'VIC_password': 'nothing'
-    }
-    with patch.dict('os.environ', env_vars):
-        client =  app.test_client()
-        yield client
-        
-        # one of the tests writes to the banner files. erase them.
-        with open('message.txt', 'w') as banner:
-            banner = banner.write('')
+    client =  app.test_client()
+    yield client
+    
+    # one of the tests writes to the banner files. erase them.
+    with open('message.txt', 'w') as banner:
+        banner = banner.write('')
 
-        with open('bannerColor.txt', 'w') as banner:
-            banner = banner.write('')
+    with open('bannerColor.txt', 'w') as banner:
+        banner = banner.write('')
 
 '''
 GET real GET routes
@@ -31,6 +26,10 @@ def test_home_get_1(client: FlaskClient):
     '''home/landing route'''
     resp = client.get('/')
     assert resp.status_code == 200
+
+def test_booth_get_1(client: FlaskClient):
+    response = client.get('/booth')
+    assert response.status_code == 200
 
 def test_boothBanner_get_1(client: FlaskClient):
     response = client.get('/booth/banner')
@@ -120,6 +119,22 @@ def test_banner_message(client: FlaskClient):
                             })
     assert check_banner() == message
 
+def test_weather_post_1(client: FlaskClient):
+    response = client.post('/booth/weather')
+    assert response.status_code == 200
+
+def test_weather_post_2(client: FlaskClient):
+    response = client.post('/booth/weather')
+    assert response.content_type == 'application/json'
+
+def test_booth_banner_content_post_1(client: FlaskClient):
+    response = client.post('/booth/banner/content')
+    assert response.status_code == 200
+
+def test_booth_banner_content_post_2(client: FlaskClient):
+    response = client.post('/booth/banner/content')
+    assert response.content_type == 'application/json'
+
 def test_stream_post_1(client: FlaskClient):
     response = client.post('/stream')
     assert response.status_code == 200
@@ -135,24 +150,6 @@ def test_stream_status_post_1(client: FlaskClient):
 
 def test_stream_status_post_2(client: FlaskClient):
     response = client.post('stream/status')
-    assert response.content_type == 'application/json'
-
-def test_statusStream_post_1(client: FlaskClient):
-    '''requires auth'''
-    response = client.post('status/stream')
-    assert response.status_code == 401
-
-def test_statusStream_post_2(client: FlaskClient):
-    '''requires auth. if auth not present, returns plain text response'''
-    response = client.post('status/stream')
-    assert 'text/html' in response.content_type
-
-def test_weather_post_1(client: FlaskClient):
-    response = client.post('/booth/weather')
-    assert response.status_code == 200
-
-def test_weather_post_2(client: FlaskClient):
-    response = client.post('/booth/weather')
     assert response.content_type == 'application/json'
 
 def test_twilio_post_1(client: FlaskClient):
@@ -171,6 +168,31 @@ def test_holiday_post_2(client: FlaskClient):
     response = client.post('/booth/holiday')
     assert response.content_type == 'application/json'
 
+def test_statusStream_post_1(client: FlaskClient):
+    '''requires auth'''
+    response = client.post('status/stream')
+    assert response.status_code == 401
+
+def test_statusStream_post_2(client: FlaskClient):
+    '''requires auth. if auth not present, returns plain text response'''
+    response = client.post('status/stream')
+    assert 'text/html' in response.content_type
+
+def test_statusPing_post_1(client: FlaskClient):
+    '''requires auth'''
+    response = client.post('status/ping')
+    assert response.status_code == 401
+
+def test_statusMounts_post_1(client: FlaskClient):
+    '''requires auth'''
+    response = client.post('status/mounts')
+    assert response.status_code == 401
+
+def test_statusUserAgent_post_1(client: FlaskClient):
+    '''requires auth'''
+    response = client.post('status/useragent')
+    assert response.status_code == 401
+
 '''
 POST non-POST routes
 '''
@@ -183,4 +205,9 @@ def test_home_post_1(client: FlaskClient):
 def test_booth_post_1(client: FlaskClient):
     '''should fail for non-POST routes'''
     response = client.post('/booth')
+    assert response.status_code == 405
+
+def test_status_post_1(client: FlaskClient):
+    '''should fail for non-POST routes'''
+    response = client.post('/status')
     assert response.status_code == 405
