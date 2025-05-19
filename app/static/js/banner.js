6ff8b8e6f8c79afad72d8119b963102257761caa
weddleben <stomachworm88@gmail.com>
2025-05-19 15:38:30 -0500

    bannerColorElement no longer used/needewd

--- app/static/js/banner.js
+++ app/static/js/banner.js
@@ -22,26 +22,13 @@ function validateBanner() {
 
 async function bannerContent() {
     const url = "/booth/banner/content"
-    const options = {method: "POST"}
+    const options = { method: "POST" }
     let response = await fetch(url, options)
     let responseJSON = await response.json()
 
     const bannerMessageElement = document.getElementById("bannerMessage")
 
     bannerMessageElement.innerText = responseJSON.banner
-
-    if (responseJSON.bannerColor) {
-        bannerColorElement.value = responseJSON.bannerColor
-    }
-    else{
-        // default to red if none chosen
-        bannerColorElement.value = "#a52a2a"
-    }
-
-    // if there is no message, change the default color back to red
-    if (!responseJSON.banner) {
-        bannerColorElement.value = "#a52a2a"
-    }
 }
 
 window.onload = bannerContent()
