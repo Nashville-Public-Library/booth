89fac53af3af4d610afcb5b48c5aaa7b7e1560af
weddleben <stomachworm88@gmail.com>
2025-06-03 16:46:59 -0500

    serve home page from /pwa/pages route with the rest of the html files
    this removes our ability to use jinja, but keeps things cleaner and more logically organized

--- app/pwa/views.py
+++ app/pwa/views.py
@@ -4,7 +4,7 @@ from app import app
 
 @app.route('/pwa', methods=['GET'])
 def pwa():
-    return render_template("pwa/index.html")
+    return app.send_static_file("pwa/pages/index.html")
 
 @app.route('/pwa/sw.js')
 def service_worker():
