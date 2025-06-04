7f4de435813ea34b535ae6904d3ad4cc2d5249af
weddleben <stomachworm88@gmail.com>
2025-06-04 14:02:33 -0500

    bump version

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ntl-pwa-cache-0.0.9';
+const CACHE_NAME = 'ntl-pwa-cache-0.0.10';
 const FILES_TO_CACHE = [
     '/static/pwa/pages/index.html',
     '/static/pwa/pages/home.html',
