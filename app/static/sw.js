833a1526ae8150c0c2bbfda24a4bf44454d79e1d
weddleben <stomachworm88@gmail.com>
2025-06-09 14:38:26 -0500

    whoops

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ntl-pwa-cache-0.1.17';
+const CACHE_NAME = 'ntl-pwa-cache-0.1.18';
 const FILES_TO_CACHE = [
     '/static/pwa/pages/index.html',
     '/static/pwa/pages/home.html',
