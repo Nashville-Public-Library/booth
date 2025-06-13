264417f4f1647942b339a1f2ec91a4981cf2f9d2
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-06-13 17:04:13 -0500

    ok, set explicit height on footer and nav

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ntl-pwa-cache-0.1.27';
+const CACHE_NAME = 'ntl-pwa-cache-0.1.28';
 const FILES_TO_CACHE = [
     '/static/pwa/pages/index.html',
     '/static/pwa/pages/home.html',
