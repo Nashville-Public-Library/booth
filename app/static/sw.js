a34ad821288c72cdb691dea116e2655795aef612
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-06-13 16:53:50 -0500

    more fun with the nav

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ntl-pwa-cache-0.1.26';
+const CACHE_NAME = 'ntl-pwa-cache-0.1.27';
 const FILES_TO_CACHE = [
     '/static/pwa/pages/index.html',
     '/static/pwa/pages/home.html',
