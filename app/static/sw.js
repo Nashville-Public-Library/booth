23029b40861f2f85f57e51960c9c4bfb0b88e82f
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-06-13 17:25:40 -0500

    i will figure this out eventually

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ntl-pwa-cache-0.1.31';
+const CACHE_NAME = 'ntl-pwa-cache-0.1.32';
 const FILES_TO_CACHE = [
     '/static/pwa/pages/index.html',
     '/static/pwa/pages/home.html',
