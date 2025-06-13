58ddc499302d0afdc86c5a1edf061b17807c25c7
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-06-13 17:43:16 -0500

    still hacking away at the nav...

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ntl-pwa-cache-0.1.33';
+const CACHE_NAME = 'ntl-pwa-cache-0.1.34';
 const FILES_TO_CACHE = [
     '/static/pwa/pages/index.html',
     '/static/pwa/pages/home.html',
