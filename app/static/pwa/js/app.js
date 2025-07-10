75a4619ec1b1e5e15bd9ac713aeee3d34fe0b4e2
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-07-10 17:53:46 -0500

    giving up on that approach...seemd hacky anyway!

--- app/static/pwa/js/app.js
+++ app/static/pwa/js/app.js
@@ -42,17 +42,17 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-function fixFooterAfterFocusChange() {
-  console.log("fixing footer...")
-  const footer = document.querySelector('footer');
-  const orig = footer.style.transform;
-  footer.style.transform = 'translateY(0.5px)';
-  requestAnimationFrame(() => {
-    console.log('putting back to normal...')
-    footer.style.transform = 'none';
-  });
-}
-window.addEventListener('focus', fixFooterAfterFocusChange)
+// function fixFooterAfterFocusChange() {
+//   console.log("fixing footer...")
+//   const footer = document.querySelector('footer');
+//   const orig = footer.style.transform;
+//   footer.style.transform = 'translateY(0.5px)';
+//   requestAnimationFrame(() => {
+//     console.log('putting back to normal...')
+//     footer.style.transform = 'none';
+//   });
+// }
+// window.addEventListener('focus', fixFooterAfterFocusChange)
 
 
 
