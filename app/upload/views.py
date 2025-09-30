e4bb2208f4766a794a95b53d58db3d20ea2e6d70
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-09-30 18:10:35 -0500

    encrypt files with password

--- app/upload/views.py
+++ app/upload/views.py
@@ -5,6 +5,7 @@ import waifuvault
 import io
 
 from app import app
+from app.ev import EV
 from app.sql import SQL
 from app.auth import require_auth
 
@@ -22,11 +23,17 @@ def view_files():
 @app.route("/upload/newfile", methods=["POST"])
 def upload_to_server():
     json: dict = request.get_json() 
-    timestamp = datetime.now().strftime("%b %d, %I:%M%p")
-    sql = SQL()
-    sql.write_uploads(filename=json.get("filename"), url=json.get("url"), token=json.get("token"), timestamp=timestamp)
+    if not json.get("password") == EV().VIC_user:
+        return {"response": "bad password"}, 401
+    try:
+        # encrypt file on server
+        waifuvault.file_update(token=json.get("token"), password=EV().VIC_user, previous_password=EV().VIC_user)
 
-    return {'response': "OK"}, 200
+        timestamp = datetime.now().strftime("%b %d, %I:%M%p")
+        SQL().write_uploads(filename=json.get("filename"), url=json.get("url"), token=json.get("token"), timestamp=timestamp)
+
+        return {'response': "OK"}, 200
+    except Exception as e: return {"response": "problem on server..."}, 500
 
 @app.route("/upload/deletefile", methods=["DELETE"])
 @require_auth
@@ -36,6 +43,23 @@ def delete_file():
     sql = SQL()
     sql.delete_uploads(filename=file)
     return {"response": "OK"}, 200
+
+@app.route("/upload/setpassword")
+def set_password():
+    password = EV().VIC_user
+    upload:dict = request.get_json()
+    if upload.get("password") == password:
+        waifuvault.file_update(token=upload.get("token"), password=EV().VIC_user)
+        return {"response": "OK"}, 200
+    return {"response": "bad password"}, 401
+
+@app.route("/upload/checkpassword", methods=["POST"])
+def check_password():
+    password:dict = request.get_json()
+    password = password.get("password")
+    if password == EV().VIC_user:
+        return {"response": "OK"}, 200
+    return {"response": "bad password"}, 401
 
 @app.route("/upload/uploads", methods=["POST"])
 def uploads():
