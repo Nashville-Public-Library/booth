ca11ad63fe4d2d93ecf2e4cb0956f337926a8446
Ben Weddle <stomachworm88@gmail.com>
2026-03-10 17:27:40 -0500

    slow (but ugly) progress...

--- app/assets/views.py
+++ app/assets/views.py
@@ -1,4 +1,5 @@
 from flask import render_template
+import requests
 
 from app import app
 from app.assets.ssh import SSH
@@ -13,4 +14,11 @@ def assets():
 def assets_folder(folder):
     ssh = SSH()
     files = ssh.get_items_in_folder(folder=folder)
-    return render_template("assets_folder.html", files=files)
+    return render_template("assets_folder.html", files=files, folder=folder)
+
+@app.route("/assets/folder/<folder>/feed", methods=['POST'])
+def assets_folder_feed(folder):
+    url: str = f"https://assets.library.nashville.gov/talkinglibrary/shows/{folder}/feed.xml"
+    request = requests.get(url)
+    feed = request.text
+    return {"feed": feed}
