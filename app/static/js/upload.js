d75afd690c5f42bef4123e22267336919ba58a2e
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2026-04-15 14:29:03 -0500

    move JS to separate file instead of inline on page

---
+++ app/static/js/upload.js
@@ -0,0 +1,124 @@
+async function uploadFile() {
+    let passwordOK = await checkPassword();
+    if (!passwordOK) { return; }
+
+    if (!checkFileSelected()) { return; }
+
+    let fileExists = await fileAlreadyExistsInDB();
+    if (fileExists) { return; }
+
+    document.getElementById("uploadStatus").style.display = "block";
+
+    const file = document.getElementById("uploadFile").files[0];
+    let data = new FormData();
+    data.append("file", file)
+
+    const request = new XMLHttpRequest();
+
+    request.upload.addEventListener("progress", function (event) {
+        // Get a suitable number for the progress bar. 
+        // Do not actually reach 100% because we still have to call OUR server to put it in the DB after the file is uploaded to WaifuVault
+        const percent = ((event.loaded / event.total) * 100) - 1;
+        const rounded = Math.floor(percent);
+        document.getElementById("progress").value = rounded;
+        document.getElementById("progressPercent").innerText = rounded + "%"
+    })
+
+    request.addEventListener("load", function () {
+        const responseJSON = JSON.parse(request.responseText);
+        const password = document.getElementById("uploadPassword").value;
+
+        let submissionData = { "url": responseJSON.url, "token": responseJSON.token, "filename": file.name, "password": password };
+        uploadFileDataToDB(submissionData);
+    })
+
+    const url = "https://waifuvault.moe/rest?expires=14d";
+    request.open("PUT", url);
+    request.send(data);
+
+}
+
+async function uploadFileDataToDB(data) {
+    const url = "/upload/newfile";
+    let response = await fetch(url, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+    if (!response.ok) {
+        alert(`Could not upload ${data.filename}! Please try again or contact the tech team.`);
+        document.getElementById("uploadStatus").style.display = "none";
+    } else {
+        document.getElementById("progressPercent").innerText = "100%";
+        await new Promise(requestAnimationFrame); // allow time for updating content before alert() box
+        alert(`${data.filename} successfully uploaded!`);
+        window.location.href = window.location.href;
+    }
+}
+
+async function checkPassword() {
+    const password = document.getElementById("uploadPassword").value;
+    if (!password) {
+        modalAlert("you forgot the password");
+        return false;
+    }
+
+    let toSend = { "password": password };
+    let response = await fetch("/upload/checkpassword", {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(toSend)
+    })
+
+    if (!response.ok) {
+        modalAlert("wrong password");
+        return false;
+    } else { return true; }
+}
+
+function checkFileSelected() {
+    const file = document.getElementById("uploadFile").files[0];
+    if (!file) {
+        modalAlert("no file selected");
+        return false;
+    } else { return true; }
+}
+
+async function fileAlreadyExistsInDB() {
+    const filename = document.getElementById("uploadFile").files[0].name;
+    const data = { "filename": filename };
+    let response = await fetch("/upload/checkduplicate", {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+
+    if (response.ok) {
+        return false;
+    }
+    let responseJSON = await response.json();
+    modalAlert(responseJSON.response);
+    return true;
+}
+
+window.addEventListener("dragover", (e) => { e.preventDefault() })
+window.addEventListener("drop", handleFileDrop)
+
+function handleFileDrop(drop) {
+    drop.preventDefault();
+    let fileDrop = drop.dataTransfer.files[0]; // only allow one file at a time
+    let dataTransfer = new DataTransfer();
+    dataTransfer.items.add(fileDrop);
+    let element = document.getElementById("uploadFile");
+    element.files = dataTransfer.files;
+
+}
